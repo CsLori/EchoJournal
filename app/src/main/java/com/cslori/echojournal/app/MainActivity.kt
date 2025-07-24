6f561c6baadd78f89ee28f0a600e8dc5377c88ac
CsLori <csuhailori@gmail.com>
2025-07-24 12:28:11 +0100

    VoiceRecorder added

--- app/src/main/java/com/cslori/echojournal/app/MainActivity.kt
+++ app/src/main/java/com/cslori/echojournal/app/MainActivity.kt
@@ -1,26 +1,26 @@
-package com.cslori.echojournal
+package com.cslori.echojournal.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.cslori.echojournal.core.presentation.designsystem.theme.EchoJournalTheme
-import com.cslori.echojournal.core.presentation.designsystem.theme.bgGradient
+import com.cslori.echojournal.echos.data.recording.AndroidVoiceRecorder
 import com.cslori.echojournal.echos.presentation.echos.EchosRoot
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             EchoJournalTheme {
